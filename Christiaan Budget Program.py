cba1f2f40594bd7378277a0a1d050a17d0e1e3de
Christiaan Meyer <65686276+ChrisBoss007@users.noreply.github.com>
2020-08-10 10:53:26 +1200

    I have added appropriate comments to our exsisting code

--- Christiaan Budget Program.py
+++ Christiaan Budget Program.py
@@ -1,3 +1,4 @@
+#This function will open the second window after the intro window. it closes the old window and opens the new window where the user is asked to register or make a account.
 def PageOpen():
     intro_slide.destroy()
     Slide2 = tk.Tk()
@@ -9,6 +10,7 @@ def PageOpen():
     positionDown = int(Slide2.winfo_screenheight() / 2 - windowHeight / 2)
     Slide2.geometry("+{}+{}".format(positionRight, positionDown))
 
+    #If the user doesnt have an account they button register will open this window and will close the old window, and open the regiter window where they can register their account.
     def register():
         Slide2.destroy()
         Slide3 = tk.Tk()
@@ -20,20 +22,26 @@ def PageOpen():
         positionDown = int(Slide3.winfo_screenheight() / 2 - windowHeight / 2)
         Slide3.geometry("+{}+{}".format(positionRight, positionDown))
 
+    #If the user does have an account, this function will close the old window and open a login window once the user has clicked the login button.
     def login():
         print("Works")
 
+    #Button will run the register function once clicked.
     registerbtn = tk.Button(Slide2, text="Register", height = 10, width = 20, command=register)
     registerbtn.pack(fill=tk.Y, side=tk.LEFT, pady=30)
 
+    #Button will run the login function once clicked.
     loginbtn = tk.Button(Slide2, text="Login", height = 10, width = 20, command=login)
     loginbtn.pack(side=tk.RIGHT, fill=tk.Y, pady=30)
 
+    #A Label informing the user what each button does.
     Label1 = tk.Label(Slide2, text="Do you have an account?", bg='white', relief="sunken", borderwidth = 3, font=('times', 13))
     Label1.place(x=60, y=3)
 
+
 import tkinter as tk
 root = tk.Tk()
+#Making the introduction window.
 root.title("Welcome to Budget calculator")
 root.geometry("380x330")
 windowWidth = root.winfo_reqwidth()
@@ -42,6 +50,7 @@ positionRight = int(root.winfo_screenwidth() / 2 - windowWidth / 2)
 positionDown = int(root.winfo_screenheight() / 2 - windowHeight / 2)
 root.geometry("+{}+{}".format(positionRight, positionDown))
 
+#Making the intro message that will introduce the user to the program.
 intro_slide = root
 intro_message1 = "Welcome! \n To budget calculator"
 intro_msg = tk.Message(root, text = intro_message1, justify="center")
@@ -59,12 +68,14 @@ intro_label.pack()
 frame = tk.Frame(root)
 frame.pack()
 
+#This button is the exit button incase the user acsednetly opened the program, and will exit when clicked.
 intro_button1 = tk.Button(frame,
                   text="NO",
                   fg="red",
                   command=quit)
 intro_button1.pack(side=tk.RIGHT)
 
+#This will run the function (page open) that will start the program.
 intro_button2 = tk.Button(frame,
                          text="YES",
                          fg="green",
