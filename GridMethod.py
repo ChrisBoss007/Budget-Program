5f5902f61c8ab51c450eb0a5548cbce2bbb56a0d
Christiaan Meyer <65686276+ChrisBoss007@users.noreply.github.com>
2020-08-18 09:38:51 +1200

    This is our test file for testing if we can save code as a file, (We can)

--- GridMethod.py
+++ GridMethod.py
@@ -1,7 +1,24 @@
-#Trialling the .grid() method
-import tkinter
-root=tkinter.Tk()
-for r in range(3):
-    for c in range(4):
-        tkinter.Label(root, text='Row%s/Column%s'%(r,c), borderwidth=1).grid(row=r,column=c)
-root.mainloop()
+# importing all files  from tkinter
+from tkinter import * 
+from tkinter import ttk 
+  
+# import only asksaveasfile from filedialog 
+# which is used to save file in any extension 
+from tkinter.filedialog import asksaveasfile 
+  
+root = Tk() 
+root.geometry('200x150') 
+  
+# function to call when user press 
+# the save button, a filedialog will 
+# open and ask to save file 
+def save(): 
+    files = [('All Files', '*.*'),  
+             ('Python Files', '*.py'), 
+             ('Text Document', '*.txt')] 
+    file = asksaveasfile(filetypes = files, defaultextension = files) 
+  
+btn = ttk.Button(root, text = 'Save', command = lambda : save()) 
+btn.pack(side = TOP, pady = 20) 
+  
+mainloop()
